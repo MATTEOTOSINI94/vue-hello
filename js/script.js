c1ad28137581af0bfcc89725e37e59dcc8b10f8e
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-28 15:10:54 +0200

    mod

---
+++ js/script.js
@@ -0,0 +1,13 @@
+Vue.config.devtools = true;
+
+window.addEventListener("DOMContentLoaded", function () {
+    const app = new Vue ({
+        el :"#vueapp",
+        data: {
+            titolo:"Vue",
+            immagine: "https://cdn.iconscout.com/icon/free/png-256/vue-2336945-1982826.png"
+        }
+
+
+    })
+})
